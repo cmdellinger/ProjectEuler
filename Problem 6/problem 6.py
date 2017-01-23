dc10c03fb1f9a616c349e782773bb2d4e820a623
cmdellinger <cdellinger@Charless-MacBook-Pro.local>
2017-01-22 21:09:38 -0500

    Problem 6 Python script
    Python script that solves problem 6

---
+++ Problem 6/problem 6.py
@@ -0,0 +1,31 @@
+"""
+ProjectEuler Problem 6: Sum square difference
+Written by cmdellinger
+
+Usage:
+  problem 6.py <natural number>
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+## ------
+## solution
+## ------
+
+def square_of_sums(number = 0): # -> integer
+    ''' returns the square of the sums of natural numbers up to input number '''
+    sum = 0
+    for integer in xrange(1, number + 1):
+        sum += integer
+    return sum**2
+
+def sum_of_squares(number = 0): # -> integer
+    ''' returns the sum of the squares of natural numbers up to input number '''
+    sum = 0
+    for integer in xrange(1, number + 1):
+        sum += integer**2
+    return sum
+
+natural_number = 100
+
+print abs(sum_of_squares(natural_number) - square_of_sums(natural_number))
